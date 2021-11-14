6044d6803e5de62712e94c3246f747c9052d884f
aerox0 <maerox1@gmail.com>
2021-11-14 19:43:08 +0200

    Update debial.sh

--- debial.sh
+++ debial.sh
@@ -7,8 +7,6 @@ git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-shell
-
 nvm install node
 nvm install --lts
 
