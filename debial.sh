46a2f9c44a6a724365d343fc3ac0e1df9141321b
aerox0 <maerox1@gmail.com>
2021-11-14 19:54:03 +0200

    Update debial.sh

--- debial.sh
+++ debial.sh
@@ -1,11 +1,11 @@
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
-sudo apt install git-flow python3 wget
+sudo apt install git-flow python3 wget curl
 
 git config --global user.name "$1"
 git config --global user.email "$2"
 git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
 nvm install node
 nvm install --lts
