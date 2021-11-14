0b20d2abe8eb4f81a318d59c7dc4ae999fbd2ceb
aerox0 <maerox1@gmail.com>
2021-11-14 19:50:21 +0200

    Update debial.sh

--- debial.sh
+++ debial.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
-sudo apt install git-flow
+sudo apt install git-flow python3
 
 git config --global user.name "$1"
 git config --global user.email "$2"
